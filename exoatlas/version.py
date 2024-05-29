639af22799e7ae87602787f0bec6b79ad6577351
Zach Berta-Thompson <zach.bertathompson@colorado.edu>
2024-05-29 13:07:20 -0600

    update testing documentation

--- exoatlas/version.py
+++ exoatlas/version.py
@@ -3,3 +3,8 @@ __version__ = "0.3.3"
 
 def version():
     return __version__
+
+
+# find and replace these if building another package off this one!
+# pypi_name = 'exoplanet-atlas'
+# import_name = 'exoatlas'
